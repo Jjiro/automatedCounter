9969cbbec7cf8538dbd3bd6901c3f5458773e0e2
Jjiro <email@email.com>
2020-03-06 20:46:55 +0200

    Minor...

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 Automated Counter
 
-This is a counter that automatically counts from zero to the final number given.
-Some social media icons with numbers placed in a grid style so it can flex around when screen size changes. Basically responsive- tested on iPhone SE to iPad Pro 12.1.
+This is a social media counter that automatically counts from zero to the final number given.
+Some social media icons are with numbers&texts placed in a grid style, so they can flex around when screen size changes. Basically responsive - tested on iPhone SE to iPad Pro 12.1.
 HTML, CSS, JavaScript.
--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-window.addEventListener('DOMContentLoaded', automateCounter, false);
-function automateCounter(){
-const counters = document.querySelectorAll(".counter");
-const speed = 500; // Lower number equal faster count animation
+window.addEventListener("DOMContentLoaded", automateCounter, false);
+function automateCounter() {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 500; // Lower number equal faster count animation
 
-counters.forEach(counter => {
-  const updateCount = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText;
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
 
-    const inc = target / speed; // Lower inc = faster counting, higher to slower counting
+      const inc = target / speed; // Lower inc = faster counting, higher to slower counting
 
-    console.log(inc);
-    console.log(count);
+      // Check if target is reached
+      if (count < target) {
+        counter.innerText = count + inc; // Add inc to count and output in counter
+        setTimeout(updateCount, 1); // Call function every ms
+      } else {
+        counter.innerText = target;
+      }
+    };
 
-    // Check if target is reached
-    if (count < target) {
-      counter.innerText = count + inc; // Add inc to count and output in counter
-      setTimeout(updateCount, 1); // Call function every ms
-    } else {
-      counter.innerText = target;
-    }
-  };
-
-  updateCount();
-});
-};
+    updateCount();
+  });
+}
